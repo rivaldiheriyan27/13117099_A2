e9c8bbea63b21cb3b7c0d5fc2e60efbb823ed46c
rivaldiheriyan27 <rivalheriyan@gmail.com>
2020-12-02 11:48:08 +0800

    Tugas minggu ke 4
    Minggu ke 4

---
+++ project_ci/application/views/template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $judul_halaman;?></title>
+	<!-- css -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	<style type="text/css">
+		.margin-bottom
+		{
+			margin-bottom: 15px;
+		}
+	</style>
+
+</head>
+<body>
